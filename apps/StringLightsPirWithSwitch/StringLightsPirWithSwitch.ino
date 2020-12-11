e6165d8a7c4cfad7ed0c281d5276a5f6eb3904f8
zackees <z@zackees.com>
2020-12-11 13:58:03 -0800

    clean code

--- apps/StringLightsPirWithSwitch/StringLightsPirWithSwitch.ino
+++ apps/StringLightsPirWithSwitch/StringLightsPirWithSwitch.ino
@@ -8,9 +8,12 @@
  *  Led Strip -
  *    Pin 6 -> (Through a n-channel mosfet)
  *    
- *  In the future, can we just use the 
+ *  In the future we can remove the mosfet and use a bunch of pins linked
+ *  together.
  */
 
+
+//#define DEBUG
 
 
 #define PIN_STATUS_LED 13
@@ -25,12 +28,11 @@
 // Prints out the state each frame
 #define PRINT_STATE
 
-#define DEBUG
 
 #ifdef DEBUG
-const uint32_t on_duration = 1000ul * 10ul * 1ul;  // 10 sec timer
+#define ON_DURATION (1000ul * 10ul * 1ul)  // 10 sec timer
 #else
-const uint32_t on_duration = 1000ul * 60ul * 3ul;  // 3 min timer
+#define ON_DURATION (1000ul * 60ul * 3ul)  // 3 min timer
 #endif
 
 int16_t led_value = 0;
@@ -45,14 +47,13 @@ class AlarmTimer {
   void Trigger() {
     start_time_ = millis();
   } 
-  bool Update(uint32_t time_now, bool alarm) {
+  uint32_t Update(uint32_t time_now, bool alarm) {
     uint32_t now = time_now;
     if (alarm) {
-      static uint32_t i = 0;
       start_time_ = now;
     }
     uint32_t delta = now - start_time_;
-    return delta < on_duration;
+    return delta;
   }
 };
 
@@ -70,47 +71,58 @@ bool probe_user_switch_on() {
   return on;
 }
 
+void startup_animation() {
+  
+}
+
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(PIN_STATUS_LED, OUTPUT);
   pinMode(PIN_PIR, INPUT);
   pinMode(PIN_LED_STRIP, OUTPUT);
   Serial.begin(SERIAL_PORT_BAUD);
+
+  startup_animation();
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   const uint32_t time_now = millis();
-  bool pwr_btn_on = probe_user_switch_on();
+  const bool pwr_btn_on = probe_user_switch_on();
   static bool s_prev_pwr_btn_on = pwr_btn_on;
   if (pwr_btn_on && (pwr_btn_on != s_prev_pwr_btn_on)) {
     alarm_timer.Trigger();
   }
   s_prev_pwr_btn_on = pwr_btn_on;
 
-  bool pir_active = (HIGH == digitalRead(PIN_PIR));
+  const bool pir_active = (HIGH == digitalRead(PIN_PIR));
   digitalWrite(PIN_STATUS_LED, pir_active ? HIGH : LOW);
 
-  bool triggered = alarm_timer.Update(time_now, pir_active);
-
-  if (triggered && pwr_btn_on) {
-    led_value += 1;
-    if (led_value > 255) {
-      led_value = 255;
-    }
+  const uint32_t alarm_time = alarm_timer.Update(time_now, pir_active);
+  const uint32_t alarm_remaining = ON_DURATION > alarm_time ? ON_DURATION - alarm_time : 0;
+  
+  if (!pwr_btn_on) {
+    led_value = 0;
   } else {
-    led_value -= 1;
-    if (led_value < 0) {
-      led_value = 0;
-    }
+    if (alarm_remaining > 0) {
+      led_value += 1;
+      if (led_value > 255) {
+        led_value = 255;
+      }
+    } else {
+      led_value -= 1;
+      if (led_value < 0) {
+        led_value = 0;
+      }
+    } 
   }
   analogWrite(PIN_LED_STRIP, led_value);
 
 #ifdef PRINT_STATE
   char buff[256] = {0};
   sprintf(buff,
-         "pwr_btn_on: %d, pir_active: %d, triggered: %d, led: %d\n",
-          pwr_btn_on, pir_active, triggered, led_value);
+         "pwr_btn_on: %d, pir_active: %d, led: %d, alarm_remaining: %"PRIu32"\n",
+          pwr_btn_on, pir_active, led_value, alarm_remaining);
   Serial.print(buff);
 #endif
 
